f8449f7b575923642f4462dbf0c587b9bb96d049
Sakshi Chaudhary <scjuly69@gmail.com>
2023-06-18 16:03:44 +0530

    Add bubble sort

---
+++ Algorithms/bubbleSort.js
@@ -0,0 +1,17 @@
+/**Implements bubble sort
+ * Big-O: O(n^2)
+ */
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([2, 1, 3, 0, -1]));
